844a1662c88e70b0af9e26c484d8833a6c824a14
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2025-02-17 16:04:37 +0000

    Add page 75

--- indes.js
+++ indes.js
@@ -575,6 +575,7 @@ const urlFriendly = string.replace(/ /g, "-");
 console.log(urlFriendly);
 */
 
+/*
 const loadAndMapMembers = compose(
   combineWith(sessionStorage, "members"),
   save(sessionStorage, "members"),
@@ -587,3 +588,525 @@ const loadAndMapMembers = compose(
   renderUSMap
 );
 getFakeMembers(100).then(loadAndMapMembers);
+*/
+
+/*
+const target = document.getElementById("target");
+const wrapper = document.createElement("div");
+const headline = document.createElement("h1");
+
+wrapper.id = "welcome";
+headline.innerText = "Hello World";
+
+wrapper.appendChild(headline);
+target.appendChild(wrapper);
+*/
+
+/*
+const { render } = ReactDOM;
+const Welcome = () => (
+  <div id="welcome">
+    <h1>Hello World</h1>
+  </div>
+);
+render(<Welcome />, document.getElementById("target"));
+*/
+
+/*
+let color_lawn = {
+  title: "lawn",
+  color: "#00FF00",
+  rating: 0,
+};
+
+function rateColor(color, rating) {
+  color.rating = rating;
+  return color;
+}
+
+console.log(rateColor(color_lawn, 5).rating);
+console.log(color_lawn.rating);
+*/
+
+/*
+let color_lawn = {
+  title: "lawn",
+  color: "#00FF00",
+  rating: 0,
+};
+
+const rateColor = function (color, rating) {
+  return Object.assign({}, color, { rating: rating });
+};
+console.log(rateColor(color_lawn, 5).rating);
+console.log(color_lawn.rating);
+*/
+
+/*
+let color_lawn = {
+  title: "lawn",
+  color: "#00FF00",
+  rating: 0,
+};
+const rateColor = (color, rating) => ({
+  ...color,
+  rating,
+});
+*/
+
+/*
+let list = [{ title: "Rad Red" }, { title: "Lawn" }, { title: "Party Pink" }];
+
+const addColor = function (title, colors) {
+  colors.push({ title: title });
+  return colors;
+};
+
+console.log(addColor("Glam Green", list).length);
+console.log(list.length);
+*/
+
+/*
+let list = [{ title: "Rad Red" }, { title: "Lawn" }, { title: "Party Pink" }];
+
+const addColor = (title, array) => array.concat({ title });
+
+console.log(addColor("Glam Green", list).length);
+console.log(list.length);
+*/
+
+/*
+let list = [{ title: "Rad Red" }, { title: "Lawn" }, { title: "Party Pink" }];
+
+const addColor = (title, list) => [...list, { title }];
+
+console.log(addColor("Glam Green", list).length);
+console.log(list.length);
+*/
+
+/*
+const frederick = {
+  name: "Frederick Dounglass",
+  canRead: false,
+  canWrite: false,
+};
+
+function selEducate() {
+  frederick.canRead = true;
+  frederick.canWrite = true;
+  return frederick;
+}
+
+selEducate();
+console.log(frederick);
+*/
+
+/*
+const frederick = {
+  name: "Frederick Dounglass",
+  canRead: false,
+  canWhite: false,
+};
+
+const selEducate = (person) => {
+  person.canRead = true;
+  person.canWhite = true;
+  return person;
+};
+
+console.log(selEducate(frederick));
+console.log(frederick);
+*/
+
+/*
+const frederick = {
+  name: "Frederick Dounglass",
+  canRead: false,
+  canWrite: false,
+};
+
+const selfEducate = (person) => ({
+  ...person,
+  canRead: true,
+  canWrite: true,
+});
+
+console.log(selfEducate(frederick));
+console.log(frederick);
+*/
+
+/*
+function Header(text) {
+  let h1 = document.createElement("h1");
+  h1.innerText = text;
+  document.body.appendChild(h1);
+}
+
+Header("Header() caused side effects");
+*/
+
+/*
+const Header = (props) => <h1>{props.title}</h1>;
+*/
+
+/*
+const schools = ["Yorktown", "Washington & Liberty", "Wakefield"];
+
+console.log(schools.join(", "));
+
+const wSchools = schools.filter((school) => school[0] === "W");
+
+console.log(wSchools);
+*/
+
+/*
+const schools = ["Yorktown", "Washington & Liberty", "Wakefield"];
+
+const cutSchool = (cut, list) => list.filter((school) => school !== cut);
+
+console.log(cutSchool("Washington & Liberty", schools).join(", "));
+
+console.log(schools.join("\n"));
+*/
+
+/*
+const schools = ["Yorktown", "Washington & Liberty", "Wakefield"];
+
+const highSchools = schools.map((school) => `${school} High School`);
+
+console.log(highSchools.join("\n"));
+
+console.log(schools.join("\n"));
+*/
+
+/*
+const schools = ["Yorktown", "Washington & Liberty", "Wakefield"];
+
+const highSchools = schools.map((school) => ({ name: school }));
+
+console.log(highSchools);
+*/
+
+/*
+let schools = [
+  { name: "Yorktown" },
+  { name: "Stratford" },
+  { name: "Washington & Liberty" },
+  { name: "Wakefield" },
+];
+let updatedSchools = editName("Stratford", "HB Woodlawn", schools);
+
+console.log(updatedSchools[1]);
+console.log(schools[1]);
+
+const editName = (oldName, name, arr) =>
+  arr.map((item) => {
+    if (item.name === oldName) {
+      return {
+        ...item,
+        name,
+      };
+    } else {
+      return item;
+    }
+  });
+*/
+
+/*
+let schools = [
+  { name: "Yorktown" },
+  { name: "Stratford" },
+  { name: "Washington & Liberty" },
+  { name: "Wakefield" },
+];
+let updatedSchools = editName("Stratford", "HB Woodlawn", schools);
+
+console.log(updatedSchools[1]);
+console.log(schools[1]);
+
+const editName = (oldName, name, arr) =>
+  arr.map((item) => (item.name === oldName ? { ...item, name } : item));
+*/
+
+/*
+const schools = {
+  Yorktown: 10,
+  "Washington & Liberty": 2,
+  Wakefield: 5,
+};
+
+const schoolArray = Object.keys(schools).map((key) => ({
+  name: key,
+  wins: schools[key],
+}));
+
+console.log(schoolArray);
+*/
+
+/*
+const ages = [21, 18, 42, 40, 64, 63, 34];
+
+const maxAge = ages.reduce((max, age) => {
+  console.log(`${age} > ${max} = ${age > max}`);
+  if (age > max) {
+    return age;
+  } else {
+    return max;
+  }
+}, 0);
+
+console.log("maxAge", maxAge);
+*/
+
+/*
+const colors = [
+  {
+    id: "xekare",
+    title: "rad red",
+    rating: 3,
+  },
+  {
+    id: "jbwsof",
+    title: "bug blue",
+    rating: 2,
+  },
+  {
+    id: "prigbj",
+    title: "grizzly grey",
+    rating: 5,
+  },
+  {
+    id: "ryhbhs1",
+    title: "banana",
+    rating: 1,
+  },
+];
+
+const hashColors = colors.reduce((hash, { id, title, rating }) => {
+  hash[id] = { title, rating };
+  return hash;
+}, {});
+
+console.log(hashColors);
+*/
+
+/*
+const colors = ["red", "red", "green", "blue", "green"];
+
+const uniqueColors = colors.reduce(
+  (unique, color) =>
+    unique.indexOf(color) !== -1 ? unique : [...unique, color],
+  []
+);
+
+console.log(uniqueColors);
+*/
+
+/*
+const invokerIf = (condition, fnTrue, fnFalse) =>
+  condition ? fnTrue() : fnFalse();
+
+const showWelcome = () => console.log("Welcome!!!");
+
+const ShowUnauthorized = () => console.log("Unauthorized!!!");
+
+invokerIf(true, showWelcome, ShowUnauthorized);
+invokerIf(false, showWelcome, ShowUnauthorized);
+*/
+
+/*
+const userLogs = (userName) => (message) =>
+  console.log(`${userName} -> ${message}`);
+const log = userLogs("grandpa23");
+log("attempted to load 20 fake members");
+getFakeMembers(20).then(
+  (members) => log(`successfully loaded ${members.length} members`),
+  (error) => log("encountered an error loading members")
+);
+*/
+
+/*
+const countdown = (value, fn) => {
+  fn(value);
+  return value > 0 ? countdown(value - 1, fn) : value;
+};
+
+countdown(10, (value) => console.log(value));
+*/
+
+/*
+const coundown = (value, fn, delay = 1000) => {
+  fn(value);
+  return value > 0
+    ? setTimeout(() => coundown(value - 1, fn, delay), delay)
+    : value;
+};
+
+const log = (value) => console.log(value);
+coundown(10, log);
+*/
+
+/*
+const dan = {
+  type: "person",
+  data: {
+    gender: "male",
+    info: {
+      id: 22,
+      fullname: {
+        first: "Dan",
+        last: "Deacon",
+      },
+    },
+  },
+};
+deepPick("type", dan);
+deepPick("data.info.fullname.first", dan);
+
+const deepPick = (field, object = {}) => {
+  const [first, ...remaining] = fields.split(".");
+  return remaining.length
+    ? deepPick(remaining.join("."), object[first])
+    : object[first];
+};
+
+deepPick("data.info.fullname.first", dan);
+*/
+
+/*
+const templace = "hh:mm:ss tt";
+const clockTime = templace
+  .replace("hh", "03")
+  .replace("mm", "33")
+  .replace("ss", "33")
+  .replace("tt", "PM");
+
+console.log(clockTime);
+
+const both = (date) => appendAMPM(civilianHours(date));
+*/
+
+/*
+const both = compose(civilianHours, appendAMPM);
+both(new Date());
+
+const compose =
+  (...fns) =>
+  (arg) =>
+    fns.reduce((composed, f) => f(composed), arg);
+*/
+
+/*
+// Вывод показаний часов каждую секунду
+setInterval(logClockTime, 1000);
+
+function logClockTime() {
+  // Получение строки показания часов в формате гражданского времени
+  let time = getClockTime();
+
+  // Очистка показаний консоли и вывод показания часов
+  console.clean();
+  console.log(time);
+}
+
+function getClockTime() {
+  // Получение текущего времени
+  let date = new Date();
+  let time = "";
+}
+// Выстраивание последовательности показаний часов
+let time = {
+  hours: date.getHours(),
+  minutes: date.getMinutes(),
+  seconds: date.getSeconds(),
+  ampm: "AM",
+};
+
+// Преобразование показания часов в формат гражданского времени
+if (time.hours == 12) {
+  time.ampm = "PM";
+} else if (time.hours > 12) {
+  time.ampm = "PM";
+  time.hours -= 12;
+}
+
+// Подстановка 0 к показанию часов, чтобы получалась пара цифр
+if (time.hours < 10) {
+  time.hours = "0" + time.hours;
+}
+
+// Подстановка 0 к показанию минут, чтобы получалась пара цифр
+if (time.minutes < 10) {
+  time.minutes = "0" + time.minutes;
+}
+
+// Подстановка 0 к показанию секунд, чтобы получалась пара цифр
+if (time.seconds < 10) {
+  time.seconds = "0" + time.seconds;
+}
+
+// Придание показаниям часов формата строки "hh:mm:ss tt"
+return (
+  time.hours + ":" + time.minutes + ":" + time.seconds + ":" + " " + time.ampm
+);
+*/
+
+const oneSecond = () => 1000;
+const getCurrentTime = () => new Date();
+const clear = () => console.clear();
+const log = (message) => console.log(message);
+
+const serializeClockTime = (date) => ({
+  hours: date.getHours(),
+  minutes: date.getMinutes(),
+  second: getSeconds(),
+});
+
+const civilianHours = (clockTime) => ({
+  ...clockTime,
+  hours: clockTime.hours > 12 ? clockTime.hours - 12 : clockTime.hours,
+});
+
+const appendAMPM = (clockTime) => ({
+  ...clockTime,
+  ampm: clockTime.hours >= 12 ? "PM" : "AM",
+});
+
+const display = (target) => (time) => target(time);
+
+const formatClock = (format) => (time) =>
+  format
+    .replace("hh", time.hours)
+    .raplace("mm", time.minutes)
+    .replace("ss", time.seconds)
+    .replace("tt", time.ampm);
+
+const prependZero = (key) => (clockTime) => ({
+  ...clockTime,
+  key: clockTime[key] < 10 ? "0" + clockTime[key] : clockTime[key],
+});
+
+const convertToCivilianTime = (clockTime) =>
+  compose(appendAMPM, civilianHours)(clockTime);
+
+const doubleDigits = (civilianHours) =>
+  compose(
+    prependZero("hours"),
+    prependZero("minutes"),
+    prependZero("second")
+  )(civilianTime);
+
+const startTicking = () =>
+  setInterval(
+    compose(
+      clear,
+      getCurrentTime,
+      serializeClockTime,
+      doubleDigits,
+      formatClock("hh:mm:ss tt"),
+      display(log)
+    ),
+    oneSecond()
+  );
+
+startTicking();
